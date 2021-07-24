119a6fd9704b4078eb112df61a77ebb3b97e29cb
MKferraz <mikaelferraz777@gmail.com>
2021-07-23 21:34:42 -0300

    Projeto Calculadora Simples - Flutter

---
+++ calculadora_simples/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:calculadora_simples/Calculadora.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+runApp(CalculadoraSimples());
+}
+class CalculadoraSimples extends StatelessWidget{
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      title: "Calculadora Simples",
+      theme: new ThemeData(primarySwatch: Colors.indigo),
+      home: new Calculadora(),
+    );
+  }
+}
